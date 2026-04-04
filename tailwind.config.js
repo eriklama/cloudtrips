c314cc7c6f0d184552213622a3ee78d6ae2548cc
eriklama <61289071+eriklama@users.noreply.github.com>
2026-04-04 16:49:54 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./public/**/*.{html,js}"],
+  content: [
+    "../public/**/*.html",
+    "../public/**/*.js"
+  ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
